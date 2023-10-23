d315d162665c8eebbcfe84f5cd171b7eef0e7fae
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-24 01:44:08 +0300

    Short Veri Tipi

--- Types&Variables/Program.cs
+++ Types&Variables/Program.cs
@@ -5,14 +5,17 @@
 Console.WriteLine("Types & Variables");
 
 //int 
-
 int number1 = 1547296;
 
 // long
 long number2 = 12345420892445665;
 
+// short
+short number3 = -32768;
+
 Console.WriteLine("Number 1 is {0}", number1);
 Console.WriteLine("Number 2 is {0}", number2);
+Console.WriteLine("Number 3 is {0}", number3);
 
 
 

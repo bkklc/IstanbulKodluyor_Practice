8ca46d413fad3464babd638fb42c15667c886be0
bkklc <abkklc@gmail.com>
2023-10-24 01:52:07 +0300

    Byte Veri Tipi

--- Types&Variables/Program.cs
+++ Types&Variables/Program.cs
@@ -4,18 +4,22 @@
 // Value Types
 Console.WriteLine("Types & Variables");
 
-//int 
+//int 	-2.147.483.648 ile 2.147.483.647
 int number1 = 1547296;
 
-// long
-long number2 = 12345420892445665;
+// long -9.223.372.036.854.775.808 ile 9.223.372.036.854.775.807 // 19 basamaklı sayı
+long number2 = 922370854775807678;
 
-// short
+// short  -32.768 ile 32.767
 short number3 = -32768;
+
+// byte 0 ile 255
+byte number4 = 255;
 
 Console.WriteLine("Number 1 is {0}", number1);
 Console.WriteLine("Number 2 is {0}", number2);
 Console.WriteLine("Number 3 is {0}", number3);
+Console.WriteLine("Number 4 is {0}", number4);
 
 
 

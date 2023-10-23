9bb8609405252f996a690adccf07cf07442da63b
bkklc <abkklc@gmail.com>
2023-10-24 01:53:50 +0300

    Bool Veri Tipi

--- Types&Variables/Program.cs
+++ Types&Variables/Program.cs
@@ -4,7 +4,7 @@
 // Value Types
 Console.WriteLine("Types & Variables");
 
-//int 	-2.147.483.648 ile 2.147.483.647
+// int 	-2.147.483.648 ile 2.147.483.647
 int number1 = 1547296;
 
 // long -9.223.372.036.854.775.808 ile 9.223.372.036.854.775.807 // 19 basamaklı sayı
@@ -16,10 +16,15 @@ short number3 = -32768;
 // byte 0 ile 255
 byte number4 = 255;
 
+// boolean = true or false
+bool condition = false;
+
+
 Console.WriteLine("Number 1 is {0}", number1);
 Console.WriteLine("Number 2 is {0}", number2);
 Console.WriteLine("Number 3 is {0}", number3);
 Console.WriteLine("Number 4 is {0}", number4);
+Console.WriteLine(condition);
 
 
 

1b3ce69c0b177ad764ce28b6ab47537c10f2de95
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-24 14:39:49 +0300

    Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -5,13 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Methods");
-
+            /*
             Add();
             Add();
             var result = Add2(12,58);
-            Console.WriteLine(result);
-            Console.WriteLine("Default Parametreli Metot: " + Add3());
-            
+            */
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add4(ref number1, number2);
+
+            Console.WriteLine("Result 2: " + result2);
+            Console.WriteLine(number1);
+
+            //Console.WriteLine("Default Parametreli Metot: " + Add3());
+
         }
 
         // Method Giriş
@@ -34,6 +42,12 @@
             return result;
         }
 
+        // Ref Keyword İle Çalışmak
+        static int Add4(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
 
     }
 }

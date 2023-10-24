fd050386fdd07387c94c4a81ac6472304172fde4
bkklc <abkklc@gmail.com>
2023-10-24 14:25:48 +0300

    Default Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,21 +10,30 @@
             Add();
             var result = Add2(12,58);
             Console.WriteLine(result);
+            Console.WriteLine("Default Parametreli Metot: " + Add3());
             
         }
 
+        // Method Giriş
         static void Add()
         {
             Console.WriteLine("Added !!");
         }
 
         // parametreli metotlar
-
         static int Add2(int number1, int number2)
         {
             var result = number1 + number2;
             return result;
         }
+
+        // Default Parametreli Metotlarla Çalışmak
+        static int Add3(int number1 = 15, int number2 = 32)
+        {
+            var result = number1 + number2;
+            return result;
+        }
+
 
     }
 }

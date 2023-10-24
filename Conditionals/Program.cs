886e50e789c9651e9b1c2ed41c38be5b033147b1
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-24 13:43:46 +0300

    Switch Bloğuyla Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -8,6 +8,7 @@ var number = 21;
 Console.WriteLine(number == 10 ? "Number is 10" : "Number is not 10");
 */
 
+/*
 if (number == 10)
 {
     Console.WriteLine("Number is 10");
@@ -21,4 +22,20 @@ else if (number == 20)
 else
 {
     Console.WriteLine("Number is not 10 or 20");
+}
+*/
+
+
+// switch-case
+switch (number)
+{
+    case 10:
+        Console.WriteLine("Number is 10");
+        break;
+    case 20:
+        Console.WriteLine("Number is 20");
+        break;
+    default:
+        Console.WriteLine("Number is not 10 or 20");
+        break;  
 }

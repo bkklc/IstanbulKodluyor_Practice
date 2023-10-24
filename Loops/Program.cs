4629d8137708ac1d2ed88023736d23ae10e2f319
bkklc <abkklc@gmail.com>
2023-10-24 23:04:43 +0300

    For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("!!! Finished !!!");
+        }
+    }
+}

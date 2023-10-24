730164b64153c8c08a4c2bfba14fed3b1e489c4c
bkklc <abkklc@gmail.com>
2023-10-25 01:39:09 +0300

    String İle Çalışmaya Başlamak

---
+++ Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Strings");
+
+            string city = "Kocaeli";
+            // string bir char arrayidir.
+            // Console.WriteLine(city[0]);
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            string city2 = "İstanbul";
+
+            string result = city + city2;
+            //Console.WriteLine(result);
+            Console.WriteLine(String.Format("{0},{1}", city,city2));
+        }
+    }
+}

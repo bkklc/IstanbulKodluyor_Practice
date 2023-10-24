8bf3829ce19033b1c6e63f5b9148828145b03548
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-25 00:14:08 +0300

    While Döngüsü

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -4,6 +4,25 @@
     {
         static void Main(string[] args)
         {
+            // ForLoop();
+
+            // While Döngüsü
+            int number = 100;
+            while (number >= 0)
+            {
+                Console.WriteLine(number);
+                number--;
+            }
+
+            Console.WriteLine("Number is " + number);
+
+
+
+        }
+
+        private static void ForLoop()
+        {
+            // For Döngüsü
             for (int i = 1; i <= 100; i++)
             {
                 Console.WriteLine(i);

c2373e25e7324f2e0ba769f6a05138a8e3d19e0e
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-25 02:24:51 +0300

    Classlara Giriş

---
+++ Classes/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classes");
+
+            CustomerManager customerManager = new CustomerManager(); // classın  örneğini oluşturduk
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager(); // classın  örneğini oluşturduk
+            productManager.Add();
+            productManager.Update();
+
+        }
+    }
+}

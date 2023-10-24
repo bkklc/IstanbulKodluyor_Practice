bad596fbc694ab20cf58a998cf8b1a9e26945bd5
bkklc <abkklc@gmail.com>
2023-10-24 03:01:29 +0300

    If Bloklarıyla Çalışmaya Başlamak

---
+++ Conditionals/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Conditionals");
+
+var number = 150;
+
+if (number == 10)
+{
+    Console.WriteLine("Number is 10");
+}
+
+else
+{
+    Console.WriteLine("Number is not 10");
+}

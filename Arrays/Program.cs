382ddd033a139f03c60484d7a9a57d1e035dac86
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-24 16:55:17 +0300

    Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Array - Diziler");
+Console.WriteLine("---------------------------");
+
+
+string[] students = new string[3];
+students[0] = "Engin";
+students[1] = "Ahmet";
+students[2] = "Berk";
+
+
+string[] students2 = {"Derin", "Engin ", "Berk" };
+
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
+
+Console.WriteLine("---------------------------");
+
+
+
+foreach (var student in students2)
+{
+    Console.WriteLine(student);
+}

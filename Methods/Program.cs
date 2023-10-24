4e7896fcbaa1cb09c48a5f718b45ac4335af8013
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-24 14:03:51 +0300

    Method Giriş

---
+++ Methods/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Methods");
+
+            Add();
+            Add();
+            Add();
+            
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Added !!");
+        }
+    }
+}

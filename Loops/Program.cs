890df8eec6588969ac790a9efccdf6c9f38a6d4e
bkklc <abkklc@gmail.com>
2023-10-25 01:24:21 +0300

    Örnek Uygulama : Asal Sayı

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -7,15 +7,42 @@
             // ForLoop();
             // WhileLoop();
             // DoWhileLoop();
+            // ForEachLoop();
 
+            if (IsPrimeNumber(6))
+            {
+                Console.WriteLine("This is prime number ");
+            }
+
+            else
+            {
+                Console.WriteLine("This is not prime number ");
+            }
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+
+
+        private static void ForEachLoop()
+        {
             string[] students = new string[3] { "Derin", "Engin ", "Berk" };
 
-            foreach (var student in students )
+            foreach (var student in students)
             {
                 Console.WriteLine(student);
             }
-
-
         }
 
         private static void DoWhileLoop()

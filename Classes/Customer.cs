77d2bc0a4ba8e9ac0b59456db138c2ae87e3ba9e
bkklc <abkklc@gmail.com>
2023-10-26 23:14:24 +0300

    Encapsulation

--- Classes/Customer.cs
+++ Classes/Customer.cs
@@ -10,12 +10,22 @@ namespace Classes
     {
         // Classlar İçinde Property Tanımlamak
 
+        //field - değişken, alan tanımlama
+        //public string FirstName;
+
+        //private string _firstName;
+        //public string FirstName
+        //{
+        //    get { return "Mr " +  _firstName;} 
+        //    set { _firstName = value; }//}
+
+        //property - özellik tanımlama - classı anlatan özelliklerini anlatmak için kullanılır.
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public  string FirstName { get; set; }
         public string LastName { get; set; }
         public string City { get; set; }
 
-
+        
 
 
     }

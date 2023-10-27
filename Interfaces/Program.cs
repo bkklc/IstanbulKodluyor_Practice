1def8d40fd70860f57c4d583ebdd41e3291b04e8
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-10-27 15:04:59 +0300

    İnterfacelere Temel Giriş

---
+++ Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    //soyut nesne
+    // aynı özelliklerin tekrarlamayı önlemek için nesnelerde çağrılarak kullanılır
+    interface IPerson 
+    {
+        int Id { get; set; }
+        int FirstName { get; set; }
+        int LastName { get; set; }
+    }
+
+    //somut nesne
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+
+        public string Adress { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+}
